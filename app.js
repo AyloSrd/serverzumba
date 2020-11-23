5e4b8be4a244cfe6a585863f199c2307e71a66d1
AyloSrd <aylo.srd@gmail.com>
2020-11-23 17:17:28 +0100

    se

--- app.js
+++ app.js
@@ -141,6 +141,13 @@ app.use("/api/auth", authRouter)
 app.use("/api/lessons", lessonsRouter)
 app.use("/api/notes", notesRouter)
 
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
 // 404 Middleware
 app.use((req, res, next) => {
   const error = new Error("Ressource not found.")
